0d53b6be42340d498abf9ab3a0d5031cdae2579f
Simon Thornley <sithor@users.noreply.github.com>
2026-08-14 10:14:12 +0000

    Add Marzipano GitHub Pages site

---
+++ furneaux-stairs/app-files/data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bottom",
+      "name": "Bottom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 3.009452496408967,
+        "pitch": 0.07423227933030319,
+        "fov": 1.5467588055390848
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.931285993875032,
+          "pitch": -0.28625006354611493,
+          "rotation": 7.853981633974483,
+          "target": "1-middle"
+        },
+        {
+          "yaw": 0.8426722398413418,
+          "pitch": 0.0398328093844551,
+          "rotation": 1.5707963267948966,
+          "target": "3-top_bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-middle",
+      "name": "Middle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -2.830293736161817,
+        "pitch": 0.10478043521395008,
+        "fov": 1.5467588055390848
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1059602853218014,
+          "pitch": -0.2747342045846075,
+          "rotation": 7.0685834705770345,
+          "target": "2-top"
+        },
+        {
+          "yaw": -1.6469605033096606,
+          "pitch": 0.43783253285825197,
+          "rotation": 1.5707963267948966,
+          "target": "0-bottom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-top",
+      "name": "Top",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.15422363935804384,
+        "pitch": 0.08856441547845506,
+        "fov": 1.5467588055390848
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05338752262706947,
+          "pitch": 0.4972918191027187,
+          "rotation": 4.71238898038469,
+          "target": "1-middle"
+        },
+        {
+          "yaw": -0.9015364699271764,
+          "pitch": -0.10014250407098046,
+          "rotation": 1.5707963267948966,
+          "target": "4-bottom_bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-top_bathroom",
+      "name": "Top_bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -1.498444495984966,
+        "pitch": 0.08357487094442817,
+        "fov": 1.5467588055390848
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.684226217104781,
+          "pitch": -0.0830206715525943,
+          "rotation": 4.71238898038469,
+          "target": "0-bottom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bottom_bathroom",
+      "name": "bottom_bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -0.4379982736094554,
+        "pitch": -0.037380453877393194,
+        "fov": 1.5467588055390848
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4140018051152321,
+          "pitch": -0.024453001577615652,
+          "rotation": 4.71238898038469,
+          "target": "2-top"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Furneaux stairs",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
